807eb359158a0f2e78547d18b774f86f23ee68d0
Charles (Chaz) Formichella <74632592+chazpf@users.noreply.github.com>
2021-10-02 14:32:25 -0400

    carousel builder working

--- app.js
+++ app.js
@@ -48,25 +48,98 @@ class MonsterImgs {
 
 const monsterImgsOneFourth = new MonsterImgs(monstersCROneFourth, '1-4');
 const monsterImgsOneHalf = new MonsterImgs(monstersCROneHalf, '1-2');
-console.log(monsterImgsOneHalf);
 const monsterImgs1 = new MonsterImgs(monstersCR1, '1');
 
-$(() => {
-  for (const monster in monsterImgsOneFourth) {
-    const $img = $('<img>').attr('src', monsterImgsOneFourth[monster]);
-    $img.css('width', '20vw');
-    $('body').append($img)
-  }
-})
-
-const callAPI = (arr, cr) => {
+const callAPI = (monsterArr, imgObj, urlCR) => {
   $.ajax({
-    url: `https://api.open5e.com/monsters/?document__slug=wotc-srd&challenge_rating=${cr}`,
+    url: `https://api.open5e.com/monsters/?document__slug=wotc-srd&challenge_rating=${urlCR}`,
     type: "GET"
   }).then(data => {
-    const dataTrimmed = data.results.filter(monster => arr.includes(monster.slug))
+    const dataTrimmed = data.results.filter(monster => monsterArr.includes(monster.slug))
     console.log(dataTrimmed);
+    buildCarousel(imgObj, dataTrimmed)
   }, () => {
     console.log('Bad request');
   })
 }
+
+
+const buildCarousel = (imgObj, data) => {
+  const $carousel = $('<div>').addClass('carousel');
+
+  const $buttonsContainer = $('<div>').attr('id', 'carousel-button-container');
+  const $prevButton = $('<button>').attr('id', 'prev').text('Previous');
+  const $selectButton = $('<button>').attr('id', 'select').text('Select');
+  const $nextButton = $('<button>').attr('id', 'next').text('Next');
+
+  $buttonsContainer.append($prevButton).append($selectButton).append($nextButton);
+  $carousel.append($buttonsContainer);
+  $('body').append($carousel);
+
+  for (const monster in imgObj) {
+    const monsterData = data.find(m => m.slug === monster);
+    const $monsterContainer = buildMonsterContainer(imgObj[monster], monsterData)
+    $monsterContainer.insertBefore($buttonsContainer);
+  }
+
+  $('.carousel-monster-container').eq(0).toggleClass('hide');
+
+  let currentIndex = 0;
+  let numOfContainers = $('.carousel-monster-container').length - 1;
+  const changeCarousel = direction => {
+    $('.carousel-monster-container').eq(currentIndex).toggleClass('hide');
+    if (direction === 'next') {
+      if (currentIndex < numOfContainers) {
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+      }
+    } else if (direction === 'prev') {
+      if (currentIndex > 0) {
+        currentIndex--;
+      } else {
+        currentIndex = numOfContainers;
+      }
+    }
+    $('.carousel-monster-container').eq(currentIndex).toggleClass('hide')
+  }
+
+  $('#next').on('click', () => {
+    changeCarousel('next');
+  });
+  $('#prev').on('click', () => {
+    changeCarousel('prev');
+  });
+
+
+}
+
+const buildMonsterContainer = (img, data) => {
+  const $monsterContainer = $('<div>').addClass('carousel-monster-container').addClass('hide');
+  const $img = $('<img>').addClass('carousel-img').attr('src', img);
+  const $monsterStats = $('<div>').addClass('carousel-monster-stats');
+  const $name = $('<p>').html(`<strong>${data.name}</strong>`);
+  const $hp = $('<p>').html(`<strong>HP: </strong>${data.hit_points}`);
+  const $ac = $('<p>').html(`<strong>AC: </strong>${data.armor_class}`);
+  let attackIndex;
+  if (data.actions[0].name === "Multiattack") {
+    attackIndex = 1;
+  } else {
+    attackIndex = 0;
+  }
+  let damage_bonus;
+  if (data.actions[attackIndex].damage_bonus) {
+    damage_bonus = data.actions[attackIndex].damage_bonus;
+  } else {
+    damage_bonus = 0;
+  }
+  const $attack = $('<p>').html(`<strong>Attack: </strong>${data.actions[attackIndex].name}, +${data.actions[attackIndex].attack_bonus} to hit, ${data.actions[attackIndex].damage_dice}+${damage_bonus} dmg`);
+
+  $monsterStats.append($name).append($hp).append($ac).append($attack);
+  $monsterContainer.append($img).append($monsterStats);
+  return $monsterContainer;
+}
+
+$(() => {
+  callAPI(monstersCR1, monsterImgs1, '1');
+})
